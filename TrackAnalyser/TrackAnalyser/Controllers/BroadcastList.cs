cc3bbdf2970c3a2c61e77d3e7cea0381e22a4858
reoskaro434 <reoskaro434@gmail.com>
2021-04-13 16:57:37 +0200

    create controlers and their views

---
+++ TrackAnalyser/TrackAnalyser/Controllers/BroadcastList.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackAnalyser.Controllers
+{
+    public class BroadcastList : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
