53a3710197b49235bf60f0fcd64c757d3eec10a5
reoskaro434 <reoskaro434@gmail.com>
2021-04-18 00:15:36 +0200

    view models

---
+++ TrackAnalyser/TrackAnalyser.Models/ViewModels/TrackDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackAnalyser.Models.ViewModels
+{
+    public class TrackDetailsViewModel
+    {
+        public string Author { get; set; }
+        public Canal CurrentCanal { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+        public DateTime Begin { get; set; }
+        public IEnumerable<int> LastPlayedWeek { get; set; }
+        public IEnumerable<Canal> Canals { get; set; }
+    }
+}
