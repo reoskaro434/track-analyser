90f77c51ac2a6b7eae03e8287efb9f9455e43a68
reoskaro434 <reoskaro434@gmail.com>
2021-05-17 19:34:37 +0200

    update startup Configure method

--- TrackAnalyser/TrackAnalyser/Startup.cs
+++ TrackAnalyser/TrackAnalyser/Startup.cs
@@ -79,12 +79,10 @@ namespace TrackAnalyser
             }
             else
             {
-                //   app.UseExceptionHandler("/ExceptionError");
-                //   app.UseStatusCodePagesWithRedirects("/Error/{0}");
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
+                app.UseExceptionHandler("/ExceptionError");
+                app.UseStatusCodePagesWithRedirects("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-               // app.UseHsts();
+                app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
